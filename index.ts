263996b1cc4f958d5afe28ed8ca569d2d507cbca
Ugwu Stanley <ugwuchiagoziestanley@gmail.com>
2022-03-22 19:14:41 +0100

    added some error catching

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import getTweet from "./utils/GetTweet";
 
 import  postTweet from "./utils/PostTweet";
 
+import { wrapTwitterErrors , errors } from 'twitter-error-handler';
+
 import { successMessage , failureMessage} from './utils/GetText'
 
 import getTranslation from './utils/GetTranslation';
@@ -17,18 +19,58 @@ dotenv.config()
 const app = express()
 
 
+const stopStartStream = (stream:any): void =>{
+    stream.stop()
+    console.log("stream stopped for a longer period of 30 minutes")
+
+    const wait = setInterval(() => console.log("waiting..."), 1000 * 60 * 5)
+
+    setTimeout(() => {
+            stream.start() 
+            console.log("stream long starting...") 
+            clearInterval(wait)
+    }, 1000 *  60 * 30); 
+}
+
+
+
 const streamForTweet = async () =>{
  
     const stream = twitConfig.stream('statuses/filter', { track: '@decode_abbr'})
     
     stream.on("tweet", async function (tweet, error){
+         
+         console.log("there is a stream => from stream")
+        
+          if (error) {
+
+                 console.log("error has occurred => from stream")
+
+                try {
+                    wrapTwitterErrors( error , endpoint) 
+
+                } catch (err) {
+                    if (err instanceof errors.ProblemWithAuth) {
+                         console.log("Issue with authentication")
+                
+                    }
+
+                    else if (err instanceof errors.RateLimited) {
+                    stopStartStream(stream)
+                    }
+                    else {
+                        stopStartStream(stream)
+                    }
+                }
+          
+         }
 
         if(tweet){
 
 
-            console.log( "there is a stream");
+            console.log( "there is a tweet in stream");
            
-            let { id_str , text  , user , entities } = tweet;
+            let { id_str , text  , user , entities , retweeted_status } = tweet;
 
              const mentions = entities.user_mentions 
               //console.log(mentions)
@@ -45,23 +87,30 @@ const streamForTweet = async () =>{
 
             console.log(text, "removed mentions")
 
-            if(user?.screen_name != "decode_abbr" && text.includes("decode")){
+            if(
+                user?.screen_name != "decode_abbr" &&
+                text.toLowerCase.includes("decode") &&
+                retweeted_status == undefined 
+              ){
 
               
 
-                let text2  = text.toString().replace("@decode_abbr", '').replace("decode", '').toLowerCase();
-
+                //let text2  = text.toString().replace("@decode_abbr", '').replace("decode", '').toLowerCase();
+                //console.log(text2)
                
-                const newText = text2.replace("@decode_abbr", '').replace('decode', '').replace(/\s/g, '').toLowerCase()
-
+                const newText = text.replace("@decode_abbr", '').replace('decode', '').replace(/\s/g, '').toLowerCase()
+                console.log(newText)
                 
                 try {
                     console.log(newText, typeof newText, "text")
+
                     const result:any = await getTranslation(newText);
+
                     console.log(result, "results")
+
                     if(result) {
 
-                       const text = `${newText}: ${result}`;
+                       const text = await successMessage(newText , result);
 
                         try{
     
@@ -75,13 +124,35 @@ const streamForTweet = async () =>{
             
                             }catch(err:any){
                                 
-                                console.log(err?.allErrors[0]?.message, "=> from stream")
+                                
+                                console.log( "error occurred => from stream")
+                                console.log(err)
+
+                                
+                                        if (err === "authentication issues") {
+
+                                            console.log("Issue with authentication")
+                                        }
+
+                                        else if (err === "limited") {
+                                                stopStartStream(stream)
+                                        }
+                                        else if (err === "data") {
+                                            console.log("data issue")
+                                             stopStartStream(stream)
+                                        }
+                                        else if (err === "unknown") {
+                                            stopStartStream(stream)
+                                        }
+                                        else {
+                                            stopStartStream(stream)
+                                        }
                          }
 
                     }
                     else{
 
-                        const newText = "Sorry😢😢, I don't have this abbreviation in my catalogue. Check my bio if you want to add this word"
+                        const newText = await failureMessage()
 
                         try{
     
